fc320c3677949691c81637f444636d1f64be1941
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-02-23 14:39:48 +0530

    First_use_case

--- BasicCoreProgramms/Program.cs
+++ BasicCoreProgramms/Program.cs
@@ -6,12 +6,40 @@ using System.Threading.Tasks;
 
 namespace BasicCorePrograms;
 
- class BasicCorePrograms
- {
-    static void Main(string args[])
+class BasicCorePrograms
+{
+
+    
+    static Random random = new Random();
+
+    static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to BasicCorePrograms");
+        int count = 0;
+        int headCount = 0;
+        int tailCount = 0;
+        float headPercent = 0;
+        float tailPercent = 0;
+        double toss = 0;
+
+        do
+        {
+            Console.Write("Enter a positive integer: ");
+            count = Convert.ToInt32(Console.ReadLine());
+        } while (count < 0);
+        for (int i = 0; i < count; i++)
+        {
+            toss = random.NextDouble();
+            if (toss < 0.5)
+                headCount++;
+            else
+                tailCount++;
+        }
+        headPercent = (float)headCount / (float)(headCount + tailCount);
+        tailPercent = (float)tailCount / (float)(tailCount + headCount);
+        Console.WriteLine("Head count: " + headCount + " Head Percent: " + headPercent);
+        Console.WriteLine("Tail count: " + tailCount + " Tail Percent: " + tailPercent);
     }
- }
+}
+
 
 
